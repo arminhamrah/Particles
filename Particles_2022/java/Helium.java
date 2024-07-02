2471d4d57ebc54a53c5040b22ae81f9d21aff21a
arminhamrah <167157646+arminhamrah@users.noreply.github.com>
2024-07-01 22:30:34 -0700

    Delete Particles_2022 directory

--- Particles_2022/java/Helium.java
+++
@@ -1,9 +0,0 @@
-import java.awt.Color;
-// You are wearing special goggles that enable you to see helium in the color red.
-public class Helium extends Particle
-{
-    public Helium()
-    {
-        super(ParticlesProgram.HELIUM, Color.red);
-    }
-}
--- Particles_2022/java/MysteryParticle.java
+++
@@ -1,8 +0,0 @@
-import java.awt.Color;
-public class MysteryParticle extends Particle
-{
-    public MysteryParticle()
-    {
-        super(ParticlesProgram.MYSTERYPARTICLE, Color.blue); // change color based on particle
-    }
-}
